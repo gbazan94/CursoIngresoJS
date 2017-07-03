a16fed05c4137086ba9358862cde5033beded96b
gbazan94 <goonza.b@live.com.ar>
2017-07-03 10:32:26 -0300

    03-07-17 (Clase)

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,34 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
-
+//tomo el mes del año  
+    var mesDelAño = document.getElementById('mes').value;
+    /*Funcion Auxiliar
+    console.log ("Mes: "+mesDelAño);*/
+    switch (mesDelAño)
+    {
+        case "Enero":
+        case "Febrero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+        {
+            alert ("Falta para el invierno.");
+            break;
+        }
+        case "Julio":
+        case "Agosto":
+        {
+            alert ("Abrigate que hace frio");
+            break;
+        }
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+        {
+            alert ("Ya pasamos el frio, ahora calor!!!");
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,30 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
-
-
-
+    var mesDelAño = document.getElementById('mes').value;
+    
+    switch (mesDelAño)
+    {
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "Julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+        {
+            alert ("Este mes tiene 31 Dias.");
+            break;
+        }
+        case "Febrero":
+        {
+            alert ("Este mes tiene 28 Dias.");
+            break;
+        }
+        default:
+        {
+            alert ("Este mes tiene 30 Dias.");
+            break;
+        }
+    }
 }//FIN DE LA FUNCIÓN
